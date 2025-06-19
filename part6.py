13ac53907035ef4e81ffbbc12a70193b923b6459
thedemobro2 <48997890+thedemobro2@users.noreply.github.com>
2025-06-19 08:31:57 -0400

    Create part6.py

---
+++ part6.py
@@ -0,0 +1,9 @@
+# backend/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.database_url
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
